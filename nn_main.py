0d9b40183ebd3e513b21d79e3ca2e90c6d37da65
Amer Sulieman <asulieman@hedrick-2.local>
2019-11-03 17:48:40 -0600

    still under work my percentage different than his

--- nn_main.py
+++ nn_main.py
@@ -6,8 +6,8 @@ import random as rd
 # config values
 L = [2, 2, 2, 2]  # layers
 alpha = 0.2
-target_mse = 0.2
-max_epoch = 1000
+target_mse = 0.0001
+max_epoch = 2000
 min_error = math.inf
 min_error_epoch = -1
 epoch = 0  # one epoch is one forward and backward sweep
@@ -50,8 +50,9 @@ def build_initial_Zs(input_data):
 def build_initial_term_errors():
     layers = len(L)
     dataSet_term_erros = []
-    for layer in range(layers):
-        dataSet_term_erros.append(np.zeros((2, 1)))
+    dataSet_term_erros.append(np.zeros((2, 46)))
+    for layer in range(layers - 1):
+        dataSet_term_erros.append(np.zeros((2, 46)))
     return np.array(dataSet_term_erros)
 
 
@@ -62,7 +63,7 @@ def activation_function(T):
 
 def compute_CSqErr(real_output, predicted_output):
     CSqErr = 0
-    squared_err = (predicted_output - real_output) ** 2
+    squared_err = (real_output - predicted_output) ** 2
     # sum by column to get one row for both errors summed per prediction
     sum_of_columns = squared_err.sum(axis=0)
     CSqErr += sum_of_columns.sum()
@@ -89,11 +90,71 @@ def forward_propagate(B, Z):
     return np.array(dataSet_Ts)
 
 
-Betas = build_starting_betas()
+def compute_delta_term_for_nodes(deltas):
+    layers = len(L)
+    """
+        Compute error delta term for all nodes except input
+        1) compute error for output nodes
+        2) compute error for hidden layers
+        Note: Delta for out put node using sigmoid function:
+                Delta = (Z(output) - Y) times Z(output) times(1-Z(output))
+
+    """
+    # Compute delta for output node
+    deltas[-1] = (Z[-1] - Y) * Z[-1] * (1 - Z[-1])
+
+    # compute delta error term for rest of layers except input
+    # subtract layers by 2 to exclude last layer
+    for layer in range(layers - 2, 0, -1):
+        # Z at that layer without its bias
+        z_without_bias = Z[layer][:-1]
+        weight = z_without_bias * (1 - z_without_bias)
+        D = deltas[layer + 1].T
+        for m in range(n_samples):
+            # error summation for layer delta(L)
+            summation = (D[m, :] * betas[layer][:-1]).sum(axis=1)
+            deltas[layer][:, m] = weight[:, m] * summation
+
+
+def update_weights(betas):
+    layers = len(L)
+    for i in range(layers - 1):
+        weight = Z[i][:-1]
+        v1 = np.zeros((L[i], L[i + 1]))
+        v2 = np.zeros((1, L[i + 1]))
+        D = deltas[i + 1].T
+        for m in range(n_samples):
+            v1 += weight[:, m] @ D[m, :]
+            v2 += D[m, :]
+
+        # UPDATE BETAS WITHOUT BIAS
+        betas[i][:-1] -= (alpha / n_samples) * v1
+        # UPDATE BIAS
+        betas[i][-1, :] -= ((alpha / n_samples) * v2)[0]
+
+
+betas = build_starting_betas()
 Z = build_initial_Zs(X)
-delta = build_initial_term_errors()
-# This updates Z values and returns T values
-T_values = forward_propagate(Betas, Z)
-CSqErr = compute_CSqErr(Y, Z[-1])
-# normalize err
-CSqErr /= L[-1]
+deltas = build_initial_term_errors()
+while (mse > target_mse) and (epoch < max_epoch):
+    print("mse =", mse)
+    print("epoch = ", epoch)
+    # This updates Z values and returns T values
+    T_values = forward_propagate(betas, Z)
+    CSqErr = compute_CSqErr(Y, Z[-1])
+    # normalize err
+    CSqErr /= L[-1]
+    compute_delta_term_for_nodes(deltas)
+    update_weights(betas)
+    CSqErr /= n_samples
+    mse = CSqErr
+    epoch += 1
+    error.append(mse)
+    epo.append(epoch)
+
+    if mse < min_error:
+        min_error = mse
+        min_error_epoch = epoch
+
+print(min_error)
+print(min_error_epoch)
