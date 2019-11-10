74d22ba1dbb009e406bbd6df7e7f9b20ac885f7d
Amer Sulieman <asulieman@hedrick-2.local>
2019-11-10 17:39:26 -0600

    feature parser caleaned up, now to hotshot

--- features/features.py
+++ features/features.py
@@ -1,77 +1,95 @@
+"""
+    File takes Training Input and convert it to proper files to train with
+"""
+
 import numpy as np
+
+
+def find_diminsion(sample, diminsion):
+    """
+        Function looks for width or heigh depend on what is called wiyj
+    """
+    found_dim_start = False
+    found_dim_end = False
+    backward_counter = -1
+    # there are 16 columns/rows search from beginning and end till find first occurrance
+    for side in range(16):
+        if found_dim_start and found_dim_end:
+            break
+        if not found_dim_start:
+            if diminsion == "width":
+                dim_location = sample[:, side]
+            else:
+                dim_location = sample[side]
+            # if set has more than one, then there was other number than -1
+            if len(set(dim_location)) > 1:
+                found_dim_start = True
+                # width/column begin at that side
+                dim_start = side
+        if not found_dim_end:
+            if diminsion == "width":
+                dim_location = sample[:, backward_counter]
+            else:
+                dim_location = sample[backward_counter]
+            # if set has more than one, then there was other number than -1
+            if len(set(dim_location)) > 1:
+                found_dim_end = True
+                # 16 is because counter is negative to start from end of list
+                dim_end = 16 + backward_counter
+            else:
+                # increment backward counter for next loop
+                backward_counter -= 1
+    diminsion_needed = dim_end - dim_start
+    return diminsion_needed, dim_start, dim_end
+
+
+def find_shadded_spots(sample, height, width_start, width_end):
+    """
+        NOTE:
+        1) After finding width and height the location
+            I pick 3 spots from where sample is drawn 
+            and check how many shaded pixels it has
+        2) The 3 spots needed: hight/2, height/4, height/6
+    """
+    spots = [height // 2, height // 4, height // 6]
+    shaded = 0
+    for location in spots:
+        spot_row = sample[location][width_start : width_end + 1]
+        white_spacesss = (spot_row == -1.0).sum()
+        # IF out of 16 5 are white, then rest are shaded
+        shaded += len(spot_row) - white_spacesss
+    return shaded
+
 
 fileName = "./train.txt"
 raw_data = open(fileName, "rt")
 data = np.loadtxt(raw_data)
+# Get output column from train data
 y_output = data[:, 0]
+# delete the output column and keep features
 features_256 = np.delete(data, 0, axis=1)
+n_samples, _ = features_256.shape
 mapped_features = []
-print(y_output)
+# feature mapping
+for i in range(n_samples):
+    print(i)
+    # change row sample to columns and rows
+    sample = features_256[i].reshape((16, 16))
+    # get white spaces count for that sample
+    num_white_space = (sample == -1.0).sum()
+    width, width_start, width_end = find_diminsion(sample, diminsion="width")
+    height, height_start, height_end = find_diminsion(sample, diminsion="height")
+    feature1_ratio = width / height
+    feature2_shaded_at_locations = find_shadded_spots(
+        sample, height, width_start, width_end
+    )
+    mapped_features.append([feature1_ratio, feature2_shaded_at_locations])
+
+# save output column to a file
 np.savetxt("Y.txt", y_output)
-for x in range(features_256.shape[0]):
-    print(x)
-    # change it to columns and rows
-    number = features_256[x].reshape((16, 16))
-    # get white spaces count
-    num_white_space = (number == -1.0).sum()
-
-    # width
-    found_widthA = False
-    found_widthB = False
-    backward_counter = -1
-    for column in range(16):
-        if found_widthB and found_widthA:
-            break
-        if not found_widthA:
-            num_column = number[:, column]
-            if len(set(num_column)) > 1:
-                found_widthA = True
-                widthA = column
-        if not found_widthB:
-            num_columnB = number[:, backward_counter]
-            if len(set(num_columnB)) > 1:
-                found_widthB = True
-                widthB = 16 + backward_counter
-            else:
-                backward_counter -= 1
-    width = widthB - widthA
-
-    # height
-    found_heightA = False
-    found_heightB = False
-    backward_counter = -1
-    for row in range(16):
-        if found_heightB and found_heightA:
-            break
-        if not found_heightA:
-            if len(set(number[row])) > 1:
-                found_heightA = True
-                heightA = row
-        if not found_heightB:
-            if len(set(number[backward_counter])) > 1:
-                found_heightB = True
-                heighB = 16 + backward_counter
-            else:
-                backward_counter -= 1
-    height = heighB - heightA
-
-    # pick 3 spots from where number is drawn and check how many white spaces they have
-    # 3 spots needed: hight/2, height/4, height/6
-    spots = [height // 2, height // 4, height // 6]
-    shaded = 0
-    for spot in spots:
-        spot_row = number[spot][widthA : widthB + 1]
-        white_spacesss = (spot_row == -1.0).sum()
-        # this tells me out of that width pixles, minus white spaces and rest is shaded
-        shaded += len(spot_row) - white_spacesss
-    ratio = width / height
-    mapped_features.append([ratio, shaded])
-
-print(mapped_features)
-
-a = np.array(mapped_features)
-mat = np.matrix(a)
+new_features_array = np.array(mapped_features)
+feature_matrix = np.matrix(new_features_array)
 with open("X.txt", "wb") as f:
-    for line in mat:
+    for line in feature_matrix:
         np.savetxt(f, line, fmt="%.2f")
 
